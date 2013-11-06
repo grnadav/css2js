798f2ac3b75e9e55246d73b9c3b0f38ab90006e5
Nadav Greenberg <grnadav@gmail.com>
2013-11-06 09:35:22 +0200

    remove line reader and use simple fs read. faster

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var fs = require('fs');
-var lineReader = require('line-reader');
 var tinytim = require('tinytim');
 var argv = require('optimist')
     .usage('Usage: $0 -css [path/to/input/css] -out [path/to/out/js] -template [vanila_runner|requirejs_inject|requirejs_runner|<yours!>]')
@@ -22,20 +21,17 @@ if (!fs.existsSync(templatePath)) {
 }
 
 // read the input css and build the output js files
-var cssContent = '';
-lineReader.eachLine(cssFileUri,function (line) {
-    // add space after each line to make sure you get a valid single line css string
-    cssContent += line + ' ';
-}).then(function () {
-    // replace ' with " before wrapping with
-    cssContent = cssContent.replace(/'/g,"\'");
-    cssContent = "'" + cssContent + "'";
+var cssContent = fs.readFileSync(cssFileUri, {encoding: 'utf8'});
+cssContent = cssContent.replace(/\n/g," ");
+cssContent = cssContent.replace(/'/g,"\'");
+cssContent = "'" + cssContent + "'";
 
-    // get the injection method so it is injectable to the other templates
-    var injectCssFn = tinytim.renderFile('templates/css_injector.js.tim', {});
-    // render the output template with the css content and the injection method
-    var res = tinytim.renderFile(templatePath, {cssContent: cssContent, injectCssFn: injectCssFn});
+// get the injection method so it is injectable to the other templates
+var injectCssFn = tinytim.renderFile('templates/css_injector.js.tim', {});
+// render the output template with the css content and the injection method
+var res = tinytim.renderFile(templatePath, {cssContent: cssContent, injectCssFn: injectCssFn});
 
-    // write output file
-    fs.writeFileSync(outputFile, res, {encoding: 'utf8'});
-});
+// write output file
+fs.writeFileSync(outputFile, res, {encoding: 'utf8'});
+
+return 0;
